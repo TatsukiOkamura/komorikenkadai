01f9c2b1e61f168df6acb14fdb375000e777df45
YORUNO420 <gp22a026@oecu.jp>
2025-01-17 17:50:29 +0900

    修正

--- kadai1.py
+++ kadai1.py
@@ -5,6 +5,6 @@ a = np.array([1, 2, 3, 4])
 b = np.array([4, 3, 2, 1])
 
 # ユークリッド距離を計算
-distance = np.linalg.norm(a - b)
+A = np.linalg.norm(a - b)
 
-print("ユークリッド距離:", distance)
+print("ユークリッド距離:", A)
--- kadai3.py
+++ kadai3.py
@@ -1,15 +1,16 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 # データ
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 25, 30, 40]
+x = np.random.rand(100)
+y = np.random.rand(100)
 
 # 散布図を描画
 plt.scatter(x, y)
 
 # 軸ラベルを設定
-plt.xlabel('X軸ラベル')
-plt.ylabel('Y軸ラベル')
+plt.xlabel('Xlabel')
+plt.ylabel('Ylabel')
 
 # グリッドを表示
 plt.grid(True)
